b59cd9b04b384500e48ec0d2b15d04a963e6b254
Patrick Scoralick Costa <patrickscoralick4@gmail.com>
2016-08-11 14:56:20 -0300

    Add files via upload

---
+++ Thais_Patrick_2003_08dejulho/Thais_Patrick_2003_08dejulho/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thais_Patrick_2003_08dejulho
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+                   double sq,n;
+        Console.WriteLine("Escreva um número inteiro: ");       
+	try
+            
+                 n = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Deu erro");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+          
+        x = OddNumber < n, i++;
+            sq = n - x(n);
+      Console.WriteLine("A raiz quadrada de " + n + " é " + sq);
+      Console.ReadLine();
+}
+        }
+    }
+
